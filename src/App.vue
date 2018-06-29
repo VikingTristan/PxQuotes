5321beba2f11156c901bc3135991858ce52ec77e
Thomas Lorntzen <thomas.lorntzen@payex.com>
2018-06-29 18:25:38 +0200

    Added main vue app.

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <router-view/>
+        <vue-snotify></vue-snotify>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+};
+</script>
+
+<style>
+    @import "./assets/less/designguide-0.6.0.less";
+    /* @import "./assets/less/style.less"; */
+    @import "./assets/less/snotify.less";
+</style>

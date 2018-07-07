f2bd3ac243b4ba2bd492a1c0c03dbcc4defa1f8b
VikingTom-Main <cyrthomas1@gmail.com>
2018-07-07 13:57:36 +0200

    Added a weird reverse api baseurl condition.

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 export default () => {
     return axios.create({
-        baseURL: process.env.API
+        baseURL: process.env.API || "http://srv-captain--api"
     });
 };

54cef7d68921125de7b62db96d55b60100aafff7
Thomas Lorntzen <cyrthomas1@gmail.com>
2018-07-08 21:17:10 +0200

    Put API url into a new const.

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
+const APIUrl = process.env.API || "https://api.quotes.vikingtom.ninja";
 export default () => {
     return axios.create({
-        baseURL: process.env.API || "https://api.quotes.vikingtom.ninja"
+        baseURL: APIUrl
     });
 };
